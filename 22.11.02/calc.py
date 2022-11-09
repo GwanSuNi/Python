c118287c3bb504cf07df3c0fb7aba1860ceff1ba
GwanSuNi <tony2yoo@gmail.com>
2022-11-09 13:04:13 +0900

    [add] __name__ , __main__ practice

---
+++ 22.11.02/calc.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    return a+b
+
+def mul(a,b):
+    return a*b
+
+def print_name():
+    print(__name__)
+
+# 시작점일 때만(단독으로 프로그램 실행하는 경우) 
+# 아래 코드 작성
+if __name__ == '__main__':
+    print(add(10,20))
+    print(mul(10,20))
+    print_name()
